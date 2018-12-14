55db14b4e50d9a971d41f455120a021552b38ae0
Yi Hsiao <hsiaoyi0504@gmail.com>
2018-12-14 15:33:32 +0800

    add c++ print left and right example

---
+++ cpp/README.md
@@ -0,0 +1,3 @@
+# C++ part
+
+Usually, use `g++ <input_file_name> -o <output_file_name>` to compile.
---
+++ cpp/print_left_and_right.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main() {
+  cout << left << setw(30) << "5678"  << "=" << endl;
+  cout << left << setw(30) << "12345" << "=" << endl;
+  return 0;
+}
